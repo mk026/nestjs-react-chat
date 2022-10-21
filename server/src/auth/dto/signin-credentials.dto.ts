1c0277610c87378f6e17ba6f360f61c2592f6215
Mikhail <mk0602604@gmail.com>
2022-10-21 23:44:42 +0300

    Add global validation pipe and dto decorators

--- server/src/auth/dto/signin-credentials.dto.ts
+++ server/src/auth/dto/signin-credentials.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SigninCredentialsDto {
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
--- server/src/auth/dto/signup-credentials.dto.ts
+++ server/src/auth/dto/signup-credentials.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class SignupCredentialsDto {
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
   readonly password: string;
 }
