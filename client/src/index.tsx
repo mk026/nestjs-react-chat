071aa05938f6f83940f711f8df713f6736c1305e
Mikhail <mk0602604@gmail.com>
2022-10-21 23:13:25 +0300

    Setup react-router-dom

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(
@@ -7,6 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
