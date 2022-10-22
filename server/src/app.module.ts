a1f459094a0a70c8b99d61215efae65b28086177
Mikhail <mk0602604@gmail.com>
2022-10-22 22:03:09 +0300

    Configure typeorm root module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useFactory: typeormConfig }),
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
---
+++ server/src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export default (): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+});
