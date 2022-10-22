8c29bbcbcb560a715b1551849333ef5bbc91df8f
Mikhail <mk0602604@gmail.com>
2022-10-22 21:51:17 +0300

    Create user module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UserModule],
 })
 export class AppModule {}
