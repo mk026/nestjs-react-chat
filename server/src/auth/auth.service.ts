90c2549a5b9b32ca0c70280daa3687c8f76f3a52
Mikhail <mk0602604@gmail.com>
2022-10-23 23:52:00 +0300

    Implement generateToken method in auth service

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 import { SigninCredentialsDto } from './dto/signin-credentials.dto';
 import { SignupCredentialsDto } from './dto/signup-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
   signup(signupCredentialsDto: SignupCredentialsDto) {
     return `Signup for ${signupCredentialsDto.name}`;
   }
 
   signin(signinCredentialsDto: SigninCredentialsDto) {
     return `Signin for ${signinCredentialsDto.email}`;
+  }
+
+  genetateToken(userId: number) {
+    const payload: JwtPayload = { userId };
+    return this.jwtService.sign(payload);
   }
 }
