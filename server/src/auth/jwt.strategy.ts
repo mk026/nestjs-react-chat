b871572fa7d98cea956c7c9287fc765c12117cf0
Mikhail <mk0602604@gmail.com>
2022-10-23 23:35:05 +0300

    Create jwt strategy class

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    } as StrategyOptions);
+  }
+}
