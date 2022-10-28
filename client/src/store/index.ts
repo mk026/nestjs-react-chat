8341c43f27cdd0d3428cb002fde1ed89a1544ddb
Mikhail <mk0602604@gmail.com>
2022-10-28 23:37:51 +0300

    Add types for app store and root state

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -15,3 +15,7 @@ export const setupStore = () =>
   });
 
 export const store = setupStore();
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
