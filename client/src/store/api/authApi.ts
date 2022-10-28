d360aaee479dc9e2487a8d9f6972a75c678611c5
Mikhail <mk0602604@gmail.com>
2022-10-28 23:33:58 +0300

    Use enum to set http methods in api queries

--- client/src/store/api/authApi.ts
+++ client/src/store/api/authApi.ts
@@ -1,4 +1,4 @@
-import { baseApi } from "./baseApi";
+import { baseApi, HttpMethod } from "./baseApi";
 
 export const SIGNUP_URL = "/signup";
 export const SIGNIN_URL = "/signin";
@@ -6,10 +6,10 @@ export const SIGNIN_URL = "/signin";
 export const authApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     signup: builder.mutation({
-      query: (body) => ({ url: SIGNUP_URL, method: "POST", body }),
+      query: (body) => ({ url: SIGNUP_URL, method: HttpMethod.POST, body }),
     }),
     signin: builder.mutation({
-      query: (body) => ({ url: SIGNIN_URL, method: "POST", body }),
+      query: (body) => ({ url: SIGNIN_URL, method: HttpMethod.POST, body }),
     }),
   }),
 });
--- client/src/store/api/baseApi.ts
+++ client/src/store/api/baseApi.ts
@@ -1,5 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+export enum HttpMethod {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  PATCH = "PATCH",
+  DELETE = "DELETE",
+}
+
 export const BASE_URL = "http://localhost:3000";
 
 export const baseApi = createApi({
