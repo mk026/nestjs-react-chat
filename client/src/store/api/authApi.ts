da9c812ec529a421f3c54735f83df9618d029799
Mikhail <mk0602604@gmail.com>
2022-10-28 23:51:26 +0300

    Set auth state on fulfilled authApi mutations

--- client/src/store/api/authApi.ts
+++ client/src/store/api/authApi.ts
@@ -1,14 +1,18 @@
 import { baseApi, HttpMethod } from "./baseApi";
 
+export interface AuthResponse {
+  token: string;
+}
+
 export const SIGNUP_URL = "/signup";
 export const SIGNIN_URL = "/signin";
 
 export const authApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    signup: builder.mutation({
+    signup: builder.mutation<AuthResponse, any>({
       query: (body) => ({ url: SIGNUP_URL, method: HttpMethod.POST, body }),
     }),
-    signin: builder.mutation({
+    signin: builder.mutation<AuthResponse, any>({
       query: (body) => ({ url: SIGNIN_URL, method: HttpMethod.POST, body }),
     }),
   }),
--- client/src/store/slices/authSlice.ts
+++ client/src/store/slices/authSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { authApi } from "../api/authApi";
+
 export interface AuthState {
   isAuth: boolean;
   token: string | null;
@@ -14,4 +16,20 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addMatcher(
+      authApi.endpoints.signup.matchFulfilled,
+      (state, { payload }) => {
+        state.isAuth = true;
+        state.token = payload.token;
+      }
+    );
+    builder.addMatcher(
+      authApi.endpoints.signin.matchFulfilled,
+      (state, { payload }) => {
+        state.isAuth = true;
+        state.token = payload.token;
+      }
+    );
+  },
 });
