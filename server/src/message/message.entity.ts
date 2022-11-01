f23f5c0896c9d4dc4ba95f680ca068b6d4d2d1af
Mikhail <mk0602604@gmail.com>
2022-11-01 15:40:26 +0300

    Create message entity

---
+++ server/src/message/message.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  content: string;
+}
