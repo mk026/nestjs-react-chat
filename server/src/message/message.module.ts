4f2addb31975400dc49bc9de8446f262accc45ae
Mikhail <mk0602604@gmail.com>
2022-11-01 16:19:44 +0300

    Import typeorm module in room/message features

--- server/src/message/message.module.ts
+++ server/src/message/message.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { MessageController } from './message.controller';
+import { Message } from './message.entity';
 import { MessageService } from './message.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Message])],
   controllers: [MessageController],
   providers: [MessageService],
 })
--- server/src/room/room.module.ts
+++ server/src/room/room.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { RoomController } from './room.controller';
+import { Room } from './room.entity';
 import { RoomService } from './room.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Room])],
   controllers: [RoomController],
   providers: [RoomService],
 })
