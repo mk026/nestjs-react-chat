ac9d4d3bfb4d835624565f2959b9f40b1afa0607
Mikhail <mk0602604@gmail.com>
2022-11-02 23:49:23 +0300

    Add validation for signin-form

--- client/src/components/forms/signin-form/SigninForm.tsx
+++ client/src/components/forms/signin-form/SigninForm.tsx
@@ -1,11 +1,44 @@
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import {
+  SigninFormValues,
+  signinValidationSchema,
+} from "../../../validation/signinValidation";
 
 const SigninForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SigninFormValues>({
+    mode: "onBlur",
+    resolver: yupResolver(signinValidationSchema),
+  });
+
+  const signinHandler = (values: SigninFormValues) => {
+    console.log(values);
+  };
+
   return (
-    <Box component="form">
-      <TextField label="Email" type="email" />
-      <TextField label="Password" type="password" />
+    <Box component="form" onSubmit={handleSubmit(signinHandler)}>
+      <TextField
+        label="Email"
+        type="email"
+        {...register("email")}
+        error={!!errors.email}
+        helperText={errors.email?.message}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        {...register("password")}
+        error={!!errors.password}
+        helperText={errors.password?.message}
+      />
+      <Button type="submit">Submit</Button>
     </Box>
   );
 };
