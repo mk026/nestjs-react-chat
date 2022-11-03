dc490d8d4b739cadd92e96a8a919c70ff809d954
Mikhail <mk0602604@gmail.com>
2022-11-03 15:09:52 +0300

    Create socket conn and subscribe for msg events

--- client/src/store/api/messageApi.ts
+++ client/src/store/api/messageApi.ts
@@ -1,5 +1,6 @@
 import { baseApi } from "./baseApi";
 import { IMessage } from "../../models/IMessage";
+import { io } from "socket.io-client";
 
 export const MESSAGES_URL = "/messages";
 
@@ -7,6 +8,24 @@ export const messageApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getMessages: builder.query<IMessage[], number>({
       query: (roomId) => ({ url: MESSAGES_URL, params: { roomId } }),
+      async onCacheEntryAdded(
+        arg,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
+      ) {
+        const socket = io("ws://localhost:3000");
+        try {
+          await cacheDataLoaded;
+          socket.on("message", (data: IMessage) => {
+            updateCachedData((draft) => {
+              draft.push(data);
+            });
+          });
+        } catch (error) {
+          console.log(error);
+        }
+        await cacheEntryRemoved;
+        socket.disconnect();
+      },
     }),
   }),
 });
