e4c1bd885f9c18e5aa9b2c6a49c2b355d0e93294
Mikhail <mk0602604@gmail.com>
2022-11-04 23:54:02 +0300

    Add validation for add-room-form

--- client/src/components/forms/add-room-form/AddRoomForm.tsx
+++ client/src/components/forms/add-room-form/AddRoomForm.tsx
@@ -1,11 +1,42 @@
 import { FC } from "react";
-import { Box, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { Box, Button, TextField } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import {
+  RoomFormValues,
+  roomValidationSchema,
+} from "../../../validation/roomValidation";
 
 const AddRoomForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RoomFormValues>({
+    mode: "onBlur",
+    resolver: yupResolver(roomValidationSchema),
+  });
+
+  const addRoomHandler = (values: RoomFormValues) => {
+    console.log(values);
+  };
+
   return (
-    <Box component="form">
-      <TextField label="Room name" />
-      <TextField label="Room description" />
+    <Box component="form" onSubmit={handleSubmit(addRoomHandler)}>
+      <TextField
+        label="Room name"
+        {...register("title")}
+        error={!!errors.title}
+        helperText={errors.title?.message}
+      />
+      <TextField
+        label="Room description"
+        {...register("description")}
+        error={!!errors.description}
+        helperText={errors.description?.message}
+      />
+      <Button type="submit">Send</Button>
     </Box>
   );
 };
