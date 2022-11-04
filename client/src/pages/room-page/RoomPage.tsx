c0e6ece6958a310c729f6dfa3df088caebbc9038
Mikhail <mk0602604@gmail.com>
2022-11-04 17:09:23 +0300

    Render messages-list/add-message-form in room page

--- client/src/pages/room-page/RoomPage.tsx
+++ client/src/pages/room-page/RoomPage.tsx
@@ -1,7 +1,20 @@
 import { FC } from "react";
+import { useParams } from "react-router-dom";
+
+import AddMessageForm from "../../components/forms/add-message-form/AddMessageForm";
+import MessagesList from "../../components/messages/messages-list/MessagesList";
+import { useGetMessagesQuery } from "../../store/api/messageApi";
 
 const RoomPage: FC = () => {
-  return <div>RoomPage</div>;
+  const { id } = useParams<"id">();
+  const { data } = useGetMessagesQuery(Number(id));
+
+  return (
+    <div>
+      <MessagesList messages={data || []} />
+      <AddMessageForm />
+    </div>
+  );
 };
 
 export default RoomPage;
