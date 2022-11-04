443bf3d287c15bec31b7dc3522d6c5c3cd1ee0d4
Mikhail <mk0602604@gmail.com>
2022-11-04 23:34:22 +0300

    Subscribe to message event in chat gateway

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -1,6 +1,7 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
@@ -9,6 +10,12 @@ import { Socket } from 'socket.io';
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   handleConnection(client: Socket) {
     console.log(`Client ${client.id} connected`);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, data: string) {
+    console.log(`Client ${client.id} sended message: ${data}`);
+    return data;
   }
 
   handleDisconnect(client: Socket) {
