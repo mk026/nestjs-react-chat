75a04b0c924265a08838acc3367f2455419fbfd4
Mikhail <mk0602604@gmail.com>
2022-11-04 23:56:43 +0300

    Use constant for socket url, add types for events

--- client/src/store/api/messageApi.ts
+++ client/src/store/api/messageApi.ts
@@ -1,8 +1,26 @@
 import { baseApi } from "./baseApi";
 import { IMessage } from "../../models/IMessage";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 export const MESSAGES_URL = "/messages";
+export const SOCKET_URL = "ws://localhost:8080";
+
+export interface ServertoClientEvents {
+  message: (data: IMessage) => void;
+}
+
+export interface ClientToServerEvents {
+  message: (data: IMessage) => void;
+}
+
+export const socket: Socket<ServertoClientEvents, ClientToServerEvents> = io(
+  SOCKET_URL,
+  { autoConnect: false }
+);
+
+socket.on("connect", () => {
+  console.log("socket connected");
+});
 
 export const messageApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -12,9 +30,9 @@ export const messageApi = baseApi.injectEndpoints({
         arg,
         { updateCachedData, cacheDataLoaded, cacheEntryRemoved }
       ) {
-        const socket = io("ws://localhost:3000");
         try {
           await cacheDataLoaded;
+          socket.connect();
           socket.on("message", (data: IMessage) => {
             updateCachedData((draft) => {
               draft.push(data);
