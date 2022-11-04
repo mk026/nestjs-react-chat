14412e28de81f1a335d5bdda044abfc342cfd157
Mikhail <mk0602604@gmail.com>
2022-11-04 23:46:42 +0300

    Add validation for add-message-form

--- client/src/components/forms/add-message-form/AddMessageForm.tsx
+++ client/src/components/forms/add-message-form/AddMessageForm.tsx
@@ -1,11 +1,37 @@
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 import { Box, Button, TextField } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import {
+  MessageFormValues,
+  messageValidationSchema,
+} from "../../../validation/messageValidation";
 
 const AddMessageForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<MessageFormValues>({
+    mode: "onBlur",
+    resolver: yupResolver(messageValidationSchema),
+  });
+
+  const addMessageHandler = (values: MessageFormValues) => {
+    console.log(values);
+  };
+
   return (
-    <Box component="form">
-      <TextField placeholder="Enter your message..." multiline />
-      <Button>Send</Button>
+    <Box component="form" onSubmit={handleSubmit(addMessageHandler)}>
+      <TextField
+        multiline
+        placeholder="Enter your message..."
+        {...register("content")}
+        error={!!errors.content}
+        helperText={errors.content?.message}
+      />
+      <Button type="submit">Send</Button>
     </Box>
   );
 };
