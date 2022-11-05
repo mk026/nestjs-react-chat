2887c97015a46fcf563516ff08451b80557f3476
Mikhail <mk0602604@gmail.com>
2022-11-05 23:58:38 +0300

    Add types for getRooms query

--- client/src/store/api/roomApi.ts
+++ client/src/store/api/roomApi.ts
@@ -1,10 +1,11 @@
 import { baseApi } from "./baseApi";
+import { IRoom } from "../../models/IRoom";
 
 export const ROOMS_URL = "/rooms";
 
 export const roomApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
-    getRooms: builder.query({
+    getRooms: builder.query<IRoom[], void>({
       query: () => ({ url: ROOMS_URL }),
     }),
   }),
