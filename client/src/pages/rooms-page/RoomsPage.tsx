7cc5f15226c9cab7c35c1b12876dd55313fa9156
Mikhail <mk0602604@gmail.com>
2022-11-06 00:01:39 +0300

    Render rooms-list/search-room-form in rooms page

--- client/src/pages/rooms-page/RoomsPage.tsx
+++ client/src/pages/rooms-page/RoomsPage.tsx
@@ -1,7 +1,17 @@
 import { FC } from "react";
+import SearchRoomForm from "../../components/forms/search-room-form/SearchRoomForm";
+import RoomsList from "../../components/rooms/rooms-list/RoomsList";
+import { useGetRoomsQuery } from "../../store/api/roomApi";
 
 const RoomsPage: FC = () => {
-  return <div>RoomsPage</div>;
+  const { data } = useGetRoomsQuery();
+
+  return (
+    <div>
+      <SearchRoomForm />
+      <RoomsList rooms={data || []} />
+    </div>
+  );
 };
 
 export default RoomsPage;
