223e6f67076b436f7472258000f729de5be0281b
Mikhail <mk0602604@gmail.com>
2022-11-06 21:02:41 +0300

    Register message module in root module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import typeormConfig from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { RoomModule } from './room/room.module';
+import { MessageModule } from './message/message.module';
 import { ChatModule } from './chat/chat.module';
 
 @Module({
@@ -15,6 +16,7 @@ import { ChatModule } from './chat/chat.module';
     AuthModule,
     UserModule,
     RoomModule,
+    MessageModule,
     ChatModule,
   ],
 })
