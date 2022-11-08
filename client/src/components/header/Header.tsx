394b458779e30e395ccc98d8166eb703ff31a8fc
Mikhail <mk0602604@gmail.com>
2022-11-08 23:41:20 +0300

    Render logout button when user is signed in

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,14 +1,19 @@
 import { FC } from "react";
-import { Box, Typography } from "@mui/material";
-import { Stack } from "@mui/system";
+import { Box, Button, Stack, Typography } from "@mui/material";
+
 import Navbar from "../navbar/Navbar";
+import { useAppSelector } from "../../hooks/redux";
+import { getAuthState } from "../../store/selectors/authSelectors";
 
 const Header: FC = () => {
+  const { isAuth } = useAppSelector(getAuthState);
+
   return (
     <Box component="header">
       <Stack direction="row" alignItems="center">
         <Typography mr="2rem">React Chat</Typography>
         <Navbar />
+        {!isAuth && <Button>Logout</Button>}
       </Stack>
     </Box>
   );
