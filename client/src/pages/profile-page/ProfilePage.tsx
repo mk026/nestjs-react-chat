08f064aea2e712c918c09a6cf3278cfbe66236b0
Mikhail <mk0602604@gmail.com>
2022-11-09 23:58:58 +0300

    Render update-profile-form in profile-page

--- client/src/pages/profile-page/ProfilePage.tsx
+++ client/src/pages/profile-page/ProfilePage.tsx
@@ -1,7 +1,15 @@
 import { FC } from "react";
+import { Typography } from "@mui/material";
+
+import UpdateProfileForm from "../../components/forms/update-profile-form/UpdateProfileForm";
 
 const ProfilePage: FC = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      <Typography>ProfilePage</Typography>
+      <UpdateProfileForm />
+    </>
+  );
 };
 
 export default ProfilePage;
