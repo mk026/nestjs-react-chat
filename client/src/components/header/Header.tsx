985328e40e691f67f58adaa1d15eb697649abe15
Mikhail <mk0602604@gmail.com>
2022-11-10 22:56:35 +0300

    Add handler for signout button

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -2,18 +2,22 @@ import { FC } from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
 
 import Navbar from "../navbar/Navbar";
-import { useAppSelector } from "../../hooks/redux";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { getAuthState } from "../../store/selectors/authSelectors";
+import { signout } from "../../store/slices/authSlice";
 
 const Header: FC = () => {
   const { isAuth } = useAppSelector(getAuthState);
+  const dispatch = useAppDispatch();
+
+  const signoutHandler = () => dispatch(signout());
 
   return (
     <Box component="header">
       <Stack direction="row" alignItems="center">
         <Typography mr="2rem">React Chat</Typography>
         <Navbar />
-        {!isAuth && <Button>Logout</Button>}
+        {isAuth && <Button onClick={signoutHandler}>Logout</Button>}
       </Stack>
     </Box>
   );
