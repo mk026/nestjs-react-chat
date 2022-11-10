5f9e51c012d3c00948bdb9234781cbb33aec9577
Mikhail <mk0602604@gmail.com>
2022-11-10 23:24:24 +0300

    Redirect to home-page after signout

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
 import { Box, Button, Stack, Typography } from "@mui/material";
 
 import Navbar from "../navbar/Navbar";
@@ -6,12 +7,17 @@ import { useAppSelector } from "../../hooks/redux";
 import { getAuthState } from "../../store/selectors/authSelectors";
 import { useActions } from "../../hooks/useActions";
 import { authActions } from "../../store/slices/authSlice";
+import { Paths } from "../../routes";
 
 const Header: FC = () => {
   const { isAuth } = useAppSelector(getAuthState);
   const { signout } = useActions(authActions);
+  const navigate = useNavigate();
 
-  const signoutHandler = () => signout();
+  const signoutHandler = () => {
+    signout();
+    navigate(Paths.HOME);
+  };
 
   return (
     <Box component="header">
