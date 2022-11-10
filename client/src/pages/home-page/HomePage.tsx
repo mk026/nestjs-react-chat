f23e2aa29148fab6f2619dff454f80a1fb50efcf
Mikhail <mk0602604@gmail.com>
2022-11-10 23:20:36 +0300

    Add home-page

---
+++ client/src/pages/home-page/HomePage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Typography } from "@mui/material";
+
+const HomePage: FC = () => {
+  return (
+    <>
+      <Typography>HomePage</Typography>
+    </>
+  );
+};
+
+export default HomePage;
