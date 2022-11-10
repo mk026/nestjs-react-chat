7b689d6e63e6180e4e1f301592d3d42164b40a4e
Mikhail <mk0602604@gmail.com>
2022-11-10 21:50:02 +0300

    Add redirect buttons for signup/signin pages

--- client/src/pages/signin-page/SigninPage.tsx
+++ client/src/pages/signin-page/SigninPage.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
-import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
 
 import SigninForm from "../../components/forms/signin-form/SigninForm";
+import { Paths } from "../../routes";
 
 const SigninPage: FC = () => {
   return (
     <>
       <Typography variant="h1">SigninPage</Typography>
       <SigninForm />
+      <Typography variant="body1">
+        Don't have an account?{" "}
+        <Button component={Link} to={Paths.SIGNUP}>
+          Signup
+        </Button>
+      </Typography>
     </>
   );
 };
--- client/src/pages/signup-page/SignupPage.tsx
+++ client/src/pages/signup-page/SignupPage.tsx
@@ -1,13 +1,21 @@
 import { FC } from "react";
-import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
 
 import SignupForm from "../../components/forms/signup-form/SignupForm";
+import { Paths } from "../../routes";
 
 const SignupPage: FC = () => {
   return (
     <>
       <Typography variant="h1">SignupPage</Typography>
       <SignupForm />
+      <Typography variant="body1">
+        Already have an account?{" "}
+        <Button component={Link} to={Paths.SIGNIN}>
+          Signin
+        </Button>
+      </Typography>
     </>
   );
 };
