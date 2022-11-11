0e7014c69b691583286b1273fdb64f19b55ddf61
Mikhail <mk0602604@gmail.com>
2022-11-11 23:35:37 +0300

    Use socket join/leave methods in chat gateway

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -11,27 +11,34 @@ import { CreateMessageDto } from '../message/dto/create-message.dto';
 @WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   handleConnection(client: Socket) {
-    console.log(`Client ${client.id} connected`);
+    console.log(
+      `Client ${client.id} connected. Auth token: ${client.handshake.auth.token}`,
+    );
   }
 
   @SubscribeMessage('join')
   handleJoin(client: Socket, roomId: number) {
     console.log(`Client ${client.id} joined room: ${roomId}`);
+    client.join(roomId.toString());
     return roomId;
   }
 
   @SubscribeMessage('leave')
   handleLeave(client: Socket, roomId: number) {
     console.log(`Client ${client.id} leaved room: ${roomId}`);
+    client.leave(roomId.toString());
     return roomId;
   }
 
   @SubscribeMessage('message')
-  handleMessage(client: Socket, data: CreateMessageDto) {
+  handleMessage(client: Socket, createMessageDto: CreateMessageDto) {
     console.log(
-      `Client ${client.id} sended message: ${data.content} to room: ${data.roomId}`,
+      `Client ${client.id} sended message: ${createMessageDto.content} to room: ${createMessageDto.roomId}`,
     );
-    return data;
+    client.emit('message', createMessageDto);
+    client
+      .to(createMessageDto.roomId.toString())
+      .emit('message', createMessageDto);
   }
 
   handleDisconnect(client: Socket) {
