cc40f38fe1a1e504db145099ff1fd72493bfa411
Mikhail <mk0602604@gmail.com>
2022-11-13 21:29:33 +0300

    Add validation for update-profile-form

--- client/src/components/forms/update-profile-form/UpdateProfileForm.tsx
+++ client/src/components/forms/update-profile-form/UpdateProfileForm.tsx
@@ -1,11 +1,42 @@
-import { Box, TextField } from "@mui/material";
 import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { Box, TextField } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import {
+  UserFormValues,
+  userValidationSchema,
+} from "../../../validation/userValidation";
 
 const UpdateProfileForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormValues>({
+    mode: "onBlur",
+    resolver: yupResolver(userValidationSchema),
+  });
+
+  const updateProfileHandler = (values: UserFormValues) => {
+    console.log(values);
+  };
+
   return (
-    <Box component="form">
-      <TextField label="Name" />
-      <TextField label="Email" type="email" />
+    <Box component="form" onSubmit={handleSubmit(updateProfileHandler)}>
+      <TextField
+        label="Name"
+        {...register("name")}
+        error={!!errors.name}
+        helperText={errors.name?.message}
+      />
+      <TextField
+        label="Email"
+        type="email"
+        {...register("email")}
+        error={!!errors.email}
+        helperText={errors.email?.message}
+      />
     </Box>
   );
 };
