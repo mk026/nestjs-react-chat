0e0de56c28fb574161feab0154b703a189bb5925
Mikhail <mk0602604@gmail.com>
2022-11-13 21:56:41 +0300

    Select current user from state in profile-page

--- client/src/pages/profile-page/ProfilePage.tsx
+++ client/src/pages/profile-page/ProfilePage.tsx
@@ -2,12 +2,16 @@ import { FC } from "react";
 import { Typography } from "@mui/material";
 
 import UpdateProfileForm from "../../components/forms/update-profile-form/UpdateProfileForm";
+import { useAppSelector } from "../../hooks/redux";
+import { getAuthState } from "../../store/selectors/authSelectors";
 
 const ProfilePage: FC = () => {
+  const { user } = useAppSelector(getAuthState);
+
   return (
     <>
       <Typography>ProfilePage</Typography>
-      <UpdateProfileForm />
+      <UpdateProfileForm user={user!} />
     </>
   );
 };
