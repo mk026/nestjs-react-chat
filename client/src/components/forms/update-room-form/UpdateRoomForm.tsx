e92e50d9224e9556c95a4fa1e8728232215e5da4
Mikhail <mk0602604@gmail.com>
2022-11-14 23:54:24 +0300

    Add validation for update-room-form

--- client/src/components/forms/update-room-form/UpdateRoomForm.tsx
+++ client/src/components/forms/update-room-form/UpdateRoomForm.tsx
@@ -1,11 +1,41 @@
 import { FC } from "react";
+import { useForm } from "react-hook-form";
 import { Box, Button, TextField } from "@mui/material";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import {
+  RoomFormValues,
+  roomValidationSchema,
+} from "../../../validation/roomValidation";
 
 const UpdateRoomForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RoomFormValues>({
+    mode: "onBlur",
+    resolver: yupResolver(roomValidationSchema),
+  });
+
+  const updateRoomHandler = (values: RoomFormValues) => {
+    console.log(values);
+  };
+
   return (
-    <Box component="form">
-      <TextField label="Room name" />
-      <TextField label="Room description" />
+    <Box component="form" onSubmit={handleSubmit(updateRoomHandler)}>
+      <TextField
+        label="Room name"
+        {...register("title")}
+        error={!!errors.title}
+        helperText={errors.title?.message}
+      />
+      <TextField
+        label="Room description"
+        {...register("description")}
+        error={!!errors.description}
+        helperText={errors.description?.message}
+      />
       <Button type="submit">Update</Button>
     </Box>
   );
