c7d8090d477d13c219a1da1768199cf19c6a307f
Mikhail <mk0602604@gmail.com>
2022-11-18 22:10:32 +0300

    Fix missing dependency of auth-service
    Add user-service to exports array of user module

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -10,5 +10,6 @@ import { UserService } from './user.service';
   imports: [TypeOrmModule.forFeature([User]), forwardRef(() => AuthModule)],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
