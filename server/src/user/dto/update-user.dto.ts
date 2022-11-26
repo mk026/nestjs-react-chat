43a292143373bc49adfc7fd4348cdb1f985f6a9e
Mikhail <mk0602604@gmail.com>
2022-11-26 23:04:39 +0300

    Refactor update-user dto

--- server/src/user/dto/update-user.dto.ts
+++ server/src/user/dto/update-user.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { SignupCredentialsDto } from 'src/auth/dto/signup-credentials.dto';
 
-export class UpdateUserDto extends PartialType(SignupCredentialsDto) {}
+export class UpdateUserDto extends OmitType(PartialType(SignupCredentialsDto), [
+  'password',
+] as const) {}
