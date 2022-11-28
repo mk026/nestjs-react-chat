50d9dfdea09d4589cc449f7f1425874b28643b04
Mikhail <mk0602604@gmail.com>
2022-11-28 22:55:28 +0300

    Apply different styles for user's own messages

--- client/src/components/messages/message-item/MessageItem.tsx
+++ client/src/components/messages/message-item/MessageItem.tsx
@@ -4,14 +4,24 @@ import { Card, Typography } from "@mui/material";
 import { IMessage } from "../../../models/IMessage";
 
 import classes from "./MessageItem.module.scss";
+import { useAppSelector } from "../../../hooks/redux";
+import { getAuthState } from "../../../store/selectors/authSelectors";
 
 interface MessageItemProps {
   message: IMessage;
 }
 
 const MessageItem: FC<MessageItemProps> = ({ message }) => {
+  const { user } = useAppSelector(getAuthState);
+  const styles = [classes.message];
+  if (message.ownerId === user?.id) {
+    styles.push(classes["message--own"]);
+  } else {
+    styles.push(classes["message--other"]);
+  }
+
   return (
-    <Card classes={{ root: classes.message }}>
+    <Card classes={{ root: styles.join(" ") }}>
       <Typography variant="body1">{message.content}</Typography>
     </Card>
   );
