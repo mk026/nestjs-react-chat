b274f494563cec4ef778a0a6f10ea29263ef2e91
Mikhail <mk0602604@gmail.com>
2022-11-28 22:13:52 +0300

    Refactor message-item styles

--- client/src/components/messages/message-item/MessageItem.tsx
+++ client/src/components/messages/message-item/MessageItem.tsx
@@ -3,13 +3,15 @@ import { Card, Typography } from "@mui/material";
 
 import { IMessage } from "../../../models/IMessage";
 
+import classes from "./MessageItem.module.scss";
+
 interface MessageItemProps {
   message: IMessage;
 }
 
 const MessageItem: FC<MessageItemProps> = ({ message }) => {
   return (
-    <Card>
+    <Card classes={{ root: classes.message }}>
       <Typography variant="body1">{message.content}</Typography>
     </Card>
   );
