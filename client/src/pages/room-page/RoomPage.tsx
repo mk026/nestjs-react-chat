28c8ba98977ea1c615c615452d51493e11dc199c
Mikhail <mk0602604@gmail.com>
2022-12-01 22:01:13 +0300

    Scroll to bottom when recieving new messages

--- client/src/pages/room-page/RoomPage.tsx
+++ client/src/pages/room-page/RoomPage.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 
 import AddMessageForm from "../../components/forms/add-message-form/AddMessageForm";
 import MessagesList from "../../components/messages/messages-list/MessagesList";
@@ -9,11 +10,21 @@ const RoomPage: FC = () => {
   const { id } = useParams<"id">();
   const { data } = useGetMessagesQuery(Number(id));
 
+  const pageRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    pageRef.current?.scrollIntoView({ block: "end" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [data]);
+
   return (
-    <div>
+    <Box ref={pageRef}>
       <MessagesList messages={data || []} />
       <AddMessageForm roomId={Number(id)} />
-    </div>
+    </Box>
   );
 };
 
