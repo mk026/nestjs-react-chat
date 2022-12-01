8a3ae1ed2a6e12ca84657ccc7f3436e02a03cb2c
Mikhail <mk0602604@gmail.com>
2022-12-01 22:31:09 +0300

    Render user avatar and name in message-item

--- client/src/components/messages/message-item/MessageItem.tsx
+++ client/src/components/messages/message-item/MessageItem.tsx
@@ -1,29 +1,25 @@
 import { FC } from "react";
-import { Card, Typography } from "@mui/material";
+import { Avatar, Box, Card, Stack, Typography } from "@mui/material";
 
 import { IMessage } from "../../../models/IMessage";
 
 import classes from "./MessageItem.module.scss";
-import { useAppSelector } from "../../../hooks/redux";
-import { getAuthState } from "../../../store/selectors/authSelectors";
 
 interface MessageItemProps {
   message: IMessage;
 }
 
 const MessageItem: FC<MessageItemProps> = ({ message }) => {
-  const { user } = useAppSelector(getAuthState);
-  const styles = [classes.message];
-  if (message.ownerId === user?.id) {
-    styles.push(classes["message--own"]);
-  } else {
-    styles.push(classes["message--other"]);
-  }
-
   return (
-    <Card classes={{ root: styles.join(" ") }}>
-      <Typography variant="body1">{message.content}</Typography>
-    </Card>
+    <Box>
+      <Stack direction="row" alignItems="center">
+        <Avatar>{message.ownerId}</Avatar>
+        <Typography>Username</Typography>
+      </Stack>
+      <Card classes={{ root: classes.message }}>
+        <Typography variant="body1">{message.content}</Typography>
+      </Card>
+    </Box>
   );
 };
 
