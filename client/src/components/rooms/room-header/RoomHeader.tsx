6987ab426b0cf1eec87ff218ea0366391d791127
Mikhail <mk0602604@gmail.com>
2022-12-02 21:48:39 +0300

    Refactor room-header markup

--- client/src/components/rooms/room-header/RoomHeader.tsx
+++ client/src/components/rooms/room-header/RoomHeader.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
+import { Stack, Typography } from "@mui/material";
+
+import RoomControls from "../room-controls/RoomControls";
+import RoomMembers from "../room-members/RoomMembers";
 
 import classes from "./RoomHeader.module.scss";
 
 const RoomHeader: FC = () => {
-  return <div className={classes["room-header"]}>RoomHeader</div>;
+  return (
+    <Stack direction="row" className={classes["room-header"]}>
+      <Typography>Room name</Typography>
+      <RoomMembers />
+      <RoomControls />
+    </Stack>
+  );
 };
 
 export default RoomHeader;
