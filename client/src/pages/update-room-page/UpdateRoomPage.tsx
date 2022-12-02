32a70e2bc9fe7a99840aa733f4128504e1dae7b2
Mikhail <mk0602604@gmail.com>
2022-12-02 22:25:58 +0300

    Create update-room-page

---
+++ client/src/pages/update-room-page/UpdateRoomPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Box, Typography } from "@mui/material";
+
+const UpdateRoomPage: FC = () => {
+  return (
+    <Box>
+      <Typography>Update room</Typography>
+    </Box>
+  );
+};
+
+export default UpdateRoomPage;
