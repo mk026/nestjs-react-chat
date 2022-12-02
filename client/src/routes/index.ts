95c2f143d1ffb944100ee9d842db05af3ec52e06
Mikhail <mk0602604@gmail.com>
2022-12-02 22:37:48 +0300

    Register add/update room routes

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -4,6 +4,8 @@ import HomePage from "../pages/home-page/HomePage";
 import ProfilePage from "../pages/profile-page/ProfilePage";
 import RoomPage from "../pages/room-page/RoomPage";
 import RoomsPage from "../pages/rooms-page/RoomsPage";
+import AddRoomPage from "../pages/add-room-page/AddRoomPage";
+import UpdateRoomPage from "../pages/update-room-page/UpdateRoomPage";
 import SigninPage from "../pages/signin-page/SigninPage";
 import SignupPage from "../pages/signup-page/SignupPage";
 
@@ -14,6 +16,8 @@ export enum Paths {
   PROFILE = "/profile",
   ROOMS = "/rooms",
   ROOM = "/rooms/:id",
+  ADD_ROOM = "/rooms/add",
+  UPDATE_ROOM = "/rooms/:id/update",
 }
 
 export interface IRoute {
@@ -31,4 +35,6 @@ export const authRoutes: IRoute[] = [
   { path: Paths.PROFILE, Component: ProfilePage },
   { path: Paths.ROOMS, Component: RoomsPage },
   { path: Paths.ROOM, Component: RoomPage },
+  { path: Paths.ADD_ROOM, Component: AddRoomPage },
+  { path: Paths.UPDATE_ROOM, Component: UpdateRoomPage },
 ];
