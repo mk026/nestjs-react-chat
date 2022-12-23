5427a0332f6dc282265211a3f94b626afa235ed1
Mikhail <mk0602604@gmail.com>
2022-12-23 17:10:11 +0300

    Setup swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  setupSwagger(app);
   const configService = app.get(ConfigService);
   const port = Number(configService.get('PORT'));
   await app.listen(port);
---
+++ server/src/swagger/index.ts
@@ -0,0 +1,13 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export const setupSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Chat API')
+    .setDescription('Chat Rooms API')
+    .setVersion('1.0')
+    .addTag('chat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+};
