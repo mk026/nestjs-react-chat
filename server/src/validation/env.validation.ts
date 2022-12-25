757ca0ecd99e13e9184bf3766301125a5d971132
Mikhail <mk0602604@gmail.com>
2022-12-25 21:15:49 +0300

    Narrow type for DB_SYNC env variable

--- server/src/validation/env.validation.ts
+++ server/src/validation/env.validation.ts
@@ -23,7 +23,7 @@ export class EnvironmentVariables {
   DB_PASSWORD: string;
 
   @IsIn(['true', 'false'])
-  DB_SYNC: string;
+  DB_SYNC: 'true' | 'false';
 
   @IsNotEmpty()
   JWT_SECRET: string;
