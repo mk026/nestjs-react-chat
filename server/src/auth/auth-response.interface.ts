d391eae686ecc1a71de563246afe66ae5e6ac44b
Mikhail <mk0602604@gmail.com>
2022-12-29 20:33:16 +0300

    Fix jwt-strategy imports

--- server/src/auth/auth-response.interface.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../user/user.entity';
-
-export interface AuthResponse {
-  user: Pick<User, 'name' | 'email'>;
-  token: string;
-}
--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
 
-import { JwtPayload } from './jwt-payload.interface';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
