bb9e1269ec018afe6e9d200cb89a953afe798a2d
Mikhail <mk0602604@gmail.com>
2023-01-09 23:11:06 +0300

    Add validation for name/bio fields in signup dto

--- server/src/auth/dto/signup-credentials.dto.ts
+++ server/src/auth/dto/signup-credentials.dto.ts
@@ -8,9 +8,13 @@ import {
 
 export class SignupCredentialsDto {
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(100)
   readonly name: string;
 
   @IsOptional()
+  @MinLength(1)
+  @MaxLength(1000)
   readonly bio?: string;
 
   @IsEmail()
