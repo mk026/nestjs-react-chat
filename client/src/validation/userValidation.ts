bbca121eac35a0a54a628a305fa962bb1d18d5cd
Mikhail <mk0602604@gmail.com>
2023-01-09 23:01:56 +0300

    Refactor update-user validation schema
    Add validation for optional user-bio field

--- client/src/validation/userValidation.ts
+++ client/src/validation/userValidation.ts
@@ -4,10 +4,12 @@ import { userRules } from "./rules";
 
 export interface UserFormValues {
   name: string;
+  bio: string;
   email: string;
 }
 
 export const userValidationSchema = yup.object({
   name: yup.string().min(userRules.name.min).max(userRules.name.max).required(),
+  bio: yup.string().min(userRules.bio.min).max(userRules.bio.max).required(),
   email: yup.string().email().required(),
 });
