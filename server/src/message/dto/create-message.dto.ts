d98ee3bedb91f586dc68cad09e0cdb4a8c9fe447
Mikhail <mk0602604@gmail.com>
2023-01-09 23:18:14 +0300

    Validate fields length in create-msg/room dto's

--- server/src/message/dto/create-message.dto.ts
+++ server/src/message/dto/create-message.dto.ts
@@ -1,9 +1,11 @@
-import { IsInt, IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateMessageDto {
   @IsInt()
   readonly roomId: number;
 
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(500)
   readonly content: string;
 }
--- server/src/room/dto/create-room.dto.ts
+++ server/src/room/dto/create-room.dto.ts
@@ -1,9 +1,13 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateRoomDto {
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(100)
   readonly title: string;
 
   @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(1000)
   readonly description: string;
 }
