7e10f10b92c677e41d82f5192b4c16b2927644b8
Mikhail <mk0602604@gmail.com>
2023-01-09 23:04:22 +0300

    Fix update-user validation schema
    Make all fields optional

--- client/src/validation/userValidation.ts
+++ client/src/validation/userValidation.ts
@@ -3,13 +3,13 @@ import * as yup from "yup";
 import { userRules } from "./rules";
 
 export interface UserFormValues {
-  name: string;
-  bio: string;
-  email: string;
+  name?: string;
+  bio?: string;
+  email?: string;
 }
 
 export const userValidationSchema = yup.object({
-  name: yup.string().min(userRules.name.min).max(userRules.name.max).required(),
-  bio: yup.string().min(userRules.bio.min).max(userRules.bio.max).required(),
-  email: yup.string().email().required(),
+  name: yup.string().min(userRules.name.min).max(userRules.name.max).optional(),
+  bio: yup.string().min(userRules.bio.min).max(userRules.bio.max).optional(),
+  email: yup.string().email().optional(),
 });
