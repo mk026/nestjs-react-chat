2da95cc9ea56e24bed2aeac7e1baefafb94404ab
Mikhail <mk0602604@gmail.com>
2023-01-27 23:51:17 +0300

    Create user-info component

---
+++ client/src/components/user-info/UserInfo.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import { Paper, Typography } from "@mui/material";
+
+import { IUser } from "../../models/IUser";
+
+interface UserInfoProps {
+  user: IUser;
+}
+
+const UserInfo: FC<UserInfoProps> = ({ user }) => {
+  return (
+    <Paper>
+      <Typography>{user.name}</Typography>
+      <Typography>{user.bio}</Typography>
+      <Typography>{user.email}</Typography>
+    </Paper>
+  );
+};
+
+export default UserInfo;
