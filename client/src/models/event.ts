dc8061667e2a58a0cc6d923a6fa5607ab8538f73
Mikhail <mk0602604@gmail.com>
2023-02-01 23:36:56 +0300

    Add isTyping event in event model

--- client/src/models/event.ts
+++ client/src/models/event.ts
@@ -2,10 +2,12 @@ import { AddMessageDto, IMessage } from "./message";
 
 export interface ServerToClientEvents {
   message: (data: IMessage) => void;
+  isTyping: (name: string) => void;
 }
 
 export interface ClientToServerEvents {
   message: (data: AddMessageDto) => void;
   join: (roomId: number) => void;
   leave: (roomId: number) => void;
+  isTyping: (roomId: number) => void;
 }
