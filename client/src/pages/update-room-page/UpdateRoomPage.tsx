7a362906ebdee9d0b0dc3541c8f930d37f5f6ada
Mikhail <mk0602604@gmail.com>
2023-02-03 23:37:18 +0300

    Refactor update-room page
    Query room data and render update-room-form

--- client/src/pages/update-room-page/UpdateRoomPage.tsx
+++ client/src/pages/update-room-page/UpdateRoomPage.tsx
@@ -1,10 +1,19 @@
 import { FC } from "react";
 import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+import { useGetRoomQuery } from "../../store/api/roomApi";
+import UpdateRoomForm from "../../components/forms/update-room-form";
 
 const UpdateRoomPage: FC = () => {
+  const { id: stringId } = useParams<"id">();
+  const id = Number(stringId);
+  const { data: room } = useGetRoomQuery(id);
+
   return (
     <Box>
       <Typography>Update room</Typography>
+      {room && <UpdateRoomForm room={room} />}
     </Box>
   );
 };
