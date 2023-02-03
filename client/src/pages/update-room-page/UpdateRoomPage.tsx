6c5775cbf0c538f4fb6a6b83304f443c95d40d1e
Mikhail <mk0602604@gmail.com>
2023-02-03 23:50:43 +0300

    Redirect from upd-room page if user is not owner

--- client/src/pages/update-room-page/UpdateRoomPage.tsx
+++ client/src/pages/update-room-page/UpdateRoomPage.tsx
@@ -1,14 +1,25 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 import { useGetRoomQuery } from "../../store/api/roomApi";
 import UpdateRoomForm from "../../components/forms/update-room-form";
+import { useAppSelector } from "../../hooks/redux";
+import { getAuthState } from "../../store/selectors/authSelectors";
+import { Paths } from "../../routes";
 
 const UpdateRoomPage: FC = () => {
   const { id: stringId } = useParams<"id">();
   const id = Number(stringId);
   const { data: room } = useGetRoomQuery(id);
+  const { user } = useAppSelector(getAuthState);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user?.id !== room?.owner.id) {
+      navigate(Paths.HOME);
+    }
+  }, [user, room, navigate]);
 
   return (
     <Box>
