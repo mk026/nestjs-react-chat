0ea86d23811f1dcbc83d7291642333b008f83211
Mikhail <mk0602604@gmail.com>
2023-06-23 22:51:48 +0300

    Render app-info component at home page

--- client/src/pages/home-page/HomePage.tsx
+++ client/src/pages/home-page/HomePage.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 
 import PageTitle from "../../components/page-title";
+import AppInfo from "../../components/app-info";
 
 const HomePage: FC = () => {
   return (
     <>
       <PageTitle>HomePage</PageTitle>
+      <AppInfo />
     </>
   );
 };
