9d05b9dcc78a6326e3e3e755e89f596ba9f84b2c
Mikhail <mk0602604@gmail.com>
2023-06-24 23:30:30 +0300

    Create user-menu-button component

---
+++ client/src/components/user-menu-button/UserMenuButton.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Button } from "@mui/material";
+
+const UserMenuButton: FC = () => {
+  return <Button>User Name</Button>;
+};
+
+export default UserMenuButton;
