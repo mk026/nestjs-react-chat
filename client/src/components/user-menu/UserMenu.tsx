cb85ee9f26e865af226bc09093e134b02388a41c
Mikhail <mk0602604@gmail.com>
2023-06-24 23:57:15 +0300

    Use signout action for signout-button in user-menu

--- client/src/components/user-menu/UserMenu.tsx
+++ client/src/components/user-menu/UserMenu.tsx
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 import { Button, Stack } from "@mui/material";
 
 import { Paths } from "../../routes";
+import { useActions } from "../../hooks/useActions";
+import { authActions } from "../../store/slices/authSlice";
 
 const UserMenu: FC = () => {
+  const { signout } = useActions(authActions);
+
   return (
     <Stack>
       <Link to={Paths.PROFILE}>My Profile</Link>
       <Link to={Paths.ROOMS}>My Rooms</Link>
-      <Button>Signout</Button>
+      <Button onClick={signout}>Signout</Button>
     </Stack>
   );
 };
