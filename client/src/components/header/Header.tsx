8f0bf02a961531aef991a5159cda4461576611f6
Mikhail <mk0602604@gmail.com>
2023-06-24 23:50:18 +0300

    Refactor header component markup

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Box, Stack } from "@mui/material";
 
 import { useAppSelector } from "../../hooks/redux";
@@ -6,20 +6,29 @@ import { getAuthState } from "../../store/selectors/authSelectors";
 import AuthLinks from "../auth-links";
 import Navbar from "../navbar";
 import AppTitle from "../app-title";
-import SignoutButton from "../signout-button";
+import UserMenu from "../user-menu";
+import UserMenuButton from "../user-menu-button";
 
 import classes from "./Header.module.scss";
 
 const Header: FC = () => {
-  const { isAuth } = useAppSelector(getAuthState);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const { isAuth, user } = useAppSelector(getAuthState);
+
+  const toggleUserMenu = () => setMenuIsOpen((prev) => !prev);
 
   return (
     <Box component="header" className={classes.header} data-testid="header">
       <Stack direction="row" alignItems="center">
         <AppTitle />
         {isAuth && <Navbar />}
-        {isAuth ? <SignoutButton /> : <AuthLinks />}
+        {isAuth ? (
+          <UserMenuButton onClick={toggleUserMenu} name={user!.name} />
+        ) : (
+          <AuthLinks />
+        )}
       </Stack>
+      {menuIsOpen && <UserMenu />}
     </Box>
   );
 };
