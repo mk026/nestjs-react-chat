3ea50cf654a1a666d7554af573f04cd16d5631a9
Mikhail <mk0602604@gmail.com>
2023-06-25 22:42:59 +0300

    Add styles for page-title component

--- client/src/components/page-title/PageTitle.tsx
+++ client/src/components/page-title/PageTitle.tsx
@@ -1,8 +1,14 @@
-import { Typography } from "@mui/material";
 import { FC, PropsWithChildren } from "react";
+import { Typography } from "@mui/material";
+
+import classes from "./PageTitle.module.scss";
 
 const PageTitle: FC<PropsWithChildren> = ({ children }) => {
-  return <Typography variant="h2">{children}</Typography>;
+  return (
+    <Typography variant="h2" className={classes.title}>
+      {children}
+    </Typography>
+  );
 };
 
 export default PageTitle;
