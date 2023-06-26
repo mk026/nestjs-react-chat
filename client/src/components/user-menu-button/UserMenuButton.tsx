6e0df79d34c875c932b6f3a85f80cea8218befe7
Mikhail <mk0602604@gmail.com>
2023-06-26 22:25:59 +0300

    Add styles for user-menu-button component

--- client/src/components/user-menu-button/UserMenuButton.tsx
+++ client/src/components/user-menu-button/UserMenuButton.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 import { Button } from "@mui/material";
 
+import classes from "./UserMenuButton.module.scss";
+
 interface UserMenuButtonProps {
   onClick: () => void;
   name: string;
 }
 
 const UserMenuButton: FC<UserMenuButtonProps> = ({ onClick, name }) => {
-  return <Button onClick={onClick}>{name}</Button>;
+  return (
+    <Button className={classes.button} onClick={onClick}>
+      {name}
+    </Button>
+  );
 };
 
 export default UserMenuButton;
